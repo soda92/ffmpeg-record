f8dd94d2886cb750814446fee99d885680e60e20
SodaCris <18463922396@163.com>
2022-01-18 22:55:22 +0800

    add test code

--- test-client.cpp
+++ test-client.cpp
@@ -1,11 +1,14 @@
 #include "VideoPlay.h"
 #include <thread>
+#include <chrono>
 using namespace std;
 
 int main()
 {
     Video_StartRecord(1,
-                      (char *)"rtsp://localhost:8554/webcam", (char *)"test", (char *)"SS1", (char *)"WEBCAM");
+                      (char *)"rtsp://localhost:8554/webcam", (char *)"D:/src/ffmpeg-record/build", (char *)"SS1", (char *)"WEBCAM");
+
     std::this_thread::sleep_for(2min);
+    Video_StopRecord(1);
     return 0;
 }
